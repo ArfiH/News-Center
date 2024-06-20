a9c16f94adc5722ed640c4426bc1dc7ba64f4227
ArfiH <arfihussain27@gmail.com>
2024-06-21 00:43:07 +0530

    added responsive grid layout :lipstick:

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -3,40 +3,36 @@ import { useGlobalContext } from "../Context";
 
 const Stories = () => {
   const { hits, isLoading, removePost } = useGlobalContext();
-  
+
   if (isLoading) {
-    return (
-      <>
-        <h1>Loading.....</h1>
-      </>
-    );
+    return <h1>Loading.....</h1>;
   }
-  
+
   return (
-    <>
-      <div className="stories-div">
-        {hits.map((curPost) => {
-          const { title, author, url, urlToImage, description } = curPost;
-          return (
-            <div className="card" key={url}>
-              {urlToImage && <img className="card-img" src={urlToImage} alt={title} />}
-              <h2>{title}</h2>
-              <p>
-                By <span> {author || "Unknown"} </span> | {description}
-              </p>
-              <div className="card-button">
-                <a href={url} target="_blank" rel="noopener noreferrer">
-                  Read More
-                </a>
-                <a href="#" onClick={() => removePost(url)}>
-                  Not interested
-                </a>
-              </div>
+    <div className="stories-div">
+      {hits.map((curPost) => {
+        const { title, author, url, urlToImage, description } = curPost;
+        return (
+          <div className="card" key={url}>
+            {urlToImage && (
+              <img className="card-img" src={urlToImage} alt={title} />
+            )}
+            <h2>{title}</h2>
+            <p>
+              By <span>{author || "Unknown"}</span> | {description}
+            </p>
+            <div className="card-button">
+              <a href={url} target="_blank" rel="noopener noreferrer">
+                Read More
+              </a>
+              <a href="#" onClick={() => removePost(url)}>
+                Remove
+              </a>
             </div>
-          );
-        })}
-      </div>
-    </>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
