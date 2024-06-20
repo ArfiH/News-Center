86e57dc4476f53dc6aaa5e09a3c70f829f252903
ArfiH <arfihussain27@gmail.com>
2024-06-20 17:16:46 +0530

    added local storage 💾

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import Search from "./components/Search"
 import Pagination from "./components/Pagination";
 import Stories from "./components/Stories";
 import Toggle from "./components/Toggle";
-import { useState } from "react";
-
+import useLocalStorage from "use-local-storage";
 import "./App.css";
 
 const App = () => {
-  const [isDark, setIsDark] = useState(false);
+  const [isDark, setIsDark] = useLocalStorage("isDark", false);
   return (
     <div className="app" data-theme={isDark ? "dark" : "light"}>
       <Search />
